86d41fc83ff1ba7ef16a22fd001a3fbd84c6cb01
Chang Gue Park <chang-pa@c1r3p8.42quebec.com>
2024-04-29 16:21:04 -0400

    unittest changed

--- unittests/unittests.c
+++ unittests/unittests.c
@@ -6,49 +6,16 @@
 /*   By: chang-pa <changgyu@yonsei.ac.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 19:02:35 by chang-pa          #+#    #+#             */
-/*   Updated: 2024/04/29 14:17:15 by chang-pa         ###   ########.fr       */
+/*   Updated: 2024/04/29 16:14:43 by chang-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "unittests.h"
 
-// int		main(void)
-// {
-// 	struct termios		t;
-
-// 	if (!init_term(&t) || !init_query() || !read_char())
-// 		return (1);
-// 	return (0);
-// }
-
-// int		main(void)
-// {
-// 	int				ret;
-// 	char			*line;
-
-// 	signal(SIGINT, ut_rl_handler);
-// 	while (true)
-// 	{
-// 		line = readline("input> ");
-// 		if (line)
-// 		{
-// 			ret = strcmp(line, "bye");
-// 			if (ret)
-// 				printf("output> %s\n", line);
-// 			add_history(line);
-// 			free(line);
-// 			line = NULL;
-// 			if (!ret)
-// 				break ;
-// 		}
-// 		else
-// 			return (1);
-// 	}
-// 	return (0);
-// }
-
 int	main(int ac, char **av)
 {
+	readline_test();
 	ftast_test();
+	ncurses_test();
 	return (0);
 }
--- unittests/unittests.h
+++ unittests/unittests.h
@@ -6,7 +6,7 @@
 /*   By: chang-pa <changgyu@yonsei.ac.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 19:02:47 by chang-pa          #+#    #+#             */
-/*   Updated: 2024/04/29 14:16:46 by chang-pa         ###   ########.fr       */
+/*   Updated: 2024/04/29 16:16:51 by chang-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,26 @@
 # include <string.h>
 # include <unistd.h>
 # include <ctype.h>
-# include <libft.h>
-# include <libftast.h>
 
 # include <term.h>
 
-#include <signal.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
-#define ERROR			-1
-#define KEY_LEFT		4479771
-#define KEY_RIGHT		4414235
-#define KEY_UP			4283163
-#define KEY_DOWN		4348699
-#define KEY_BACKSPACE	127
-#define CURPOS			"\033[6n"
+# include <libft.h>
+# include <libftast.h>
 
-bool	init_term(struct termios *t);
-bool	init_query(void);
-bool	read_char(void);
-void	ut_rl_handler(int signum);
+# define ERROR			-1
+# define KEY_LEFT		4479771
+# define KEY_RIGHT		4414235
+# define KEY_UP			4283163
+# define KEY_DOWN		4348699
+# define KEY_BACKSPACE	127
+# define CURPOS			"\033[6n"
+
 void	ftast_test(void);
+void	ncurses_test(void);
+void	readline_test(void);
 
 #endif
